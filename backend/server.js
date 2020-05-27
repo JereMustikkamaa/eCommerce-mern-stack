63d28ade4b690db35916b3429610da501cedf977
JereMustikkamaa <54833968+JereMustikkamaa@users.noreply.github.com>
2020-05-27 09:30:33 +0300

    Middleware added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const cors = require('cors')
 const Product = require('./models/productModel')
 const userRoute = require('./routes/userRoute')
 const productRoute = require('./routes/productRoute')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
+const middleware = require('./middleware')
+
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
@@ -19,6 +22,8 @@ mongoose.set('useCreateIndex', true)
 mongoose.set('useUnifiedTopology', true)
 
 console.log('Connecting to mongodb')
+
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true})
   .then(result => {
     console.log('connected to MongoDB')
@@ -32,26 +37,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true , useCre
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
 
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('Server started at localhost:5000'))
